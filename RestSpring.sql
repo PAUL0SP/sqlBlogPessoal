5b63ff7e0f8fafc3101089a933e15b248956f898
PAUL0SP <87585377+PAUL0SP@users.noreply.github.com>
2022-01-18 22:00:18 -0300

    Add files via upload

---
+++ RestSpring.sql
@@ -0,0 +1,6 @@
+SELECT * FROM blog_pessoal.postagem;
+
+insert into `blog_pessoal`.`postagem` (`data`, `texto`, `titulo`)
+values ('2020-04-30 10:10:10.000', 'Estou Aprendendo', 'API Rest Spring');
+
+SELECT * FROM blog_pessoal.postagem;
